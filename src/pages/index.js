8b3deba40f4679251d845ad104028e45653a21e1
Catherine Kleimeier <catherine@masugadesign.com>
2019-12-29 18:10:41 -0500

    Add React Helmet, title attr

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 /* Site: catherinekleimeier.com */
 
-import React from "react"
-import "../css/main.scss"
+import React from "react";
+import { Helmet } from "react-helmet";
+import "../css/main.scss";
 
 const IndexPage = () => {
 	return (
-		<section className="columns">
-			<div className="column">
-				<h1>Catherine Kleimeier</h1>
-			</div>
-		</section>
+		<div className="application">
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Catherine Kleimeier</title>
+			</Helmet>
+			<section className="columns">
+				<div className="column">
+					<h1>Catherine Kleimeier</h1>
+				</div>
+			</section>
+		</div>
 	)
 }
 
