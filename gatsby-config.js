e04f356e4ed18ad2034fc7a800b01e14703a1867
Catherine Kleimeier <catherine@masugadesign.com>
2019-12-29 17:55:21 -0500

    Revert "Merge branch 'master' of https://github.com/cakleimeier/cakleimeier.github.io"
    This reverts commit 89555fef9484a040c7ff895521a5e0ea7cba9f66, reversing
    changes made to f6df7062cb498888e28dd0899d1679fe39e857b5.

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+/* Site: catherinekleimeier.com */
+
+module.exports = {
+	siteMetadata: {
+		title: `Catherine Kleimeier`,
+		siteUrl: `https://catherinekleimeier.com`,
+		description: `Personal website for Catherine Kleimeier`,
+	},
+	//pathPrefix: `/cakleimeier`,
+	plugins: [
+		`gatsby-plugin-sass`,
+		`gatsby-transformer-json`
+	],
+}
