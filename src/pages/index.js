b0aae9a7d70dc768bec1d81887d0d0506062316b
Catherine Kleimeier <catherine@masugadesign.com>
2019-12-29 17:03:12 -0500

    Add CNAME file

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+/* Site: catherinekleimeier.com */
+
+import React from "react"
+import "../css/main.scss"
+
+const IndexPage = () => {
+	return (
+		<section className="columns">
+			<div className="column">
+				<h1>Catherine Kleimeier</h1>
+			</div>
+		</section>
+	)
+}
+
+export default IndexPage
