5a275d89bb3a7c48b03e997ff6eee3d7b5c3935a
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2021-10-23 15:00:28 -0400

    Enter Experience component content

--- components/Experience.vue
+++ components/Experience.vue
@@ -1,6 +1,41 @@
 <template>
-  <section id="experience">
-    <article>
+  <section id="experience" class="col-span-7">
+    <article class="grid grid-cols-1">
+        <div class="col-span-1">
+            <h2>Experience</h2>
+            <h6 class="text-bronze"><em>Résumé available upon request</em></h6>
+        </div>
+
+        <div class="col-span-1">
+            <article>
+                <h3 class="text-grey">Full-Stack Web Developer <em class="text-sm text-bronze">with</em> <span class="text-teal">Masuga Design</span> — May 2018 to Present</h3>
+                <ul>
+                    <li>Build and maintain custom websites using Craft CMS and ExpressionEngine</li>
+                    <li>Upgrade websites from older/different Content Management Systems</li>
+                    <li>Build custom functionality to integrate with websites</li>
+                    <li>Communicate with clients to gather information, clarify and manage expectations, solve problems, and maintain relationships</li>
+                    <li>Write documentation for clients and developers</li>
+                </ul>
+            </article>
+            <article>
+                <h3 class="text-grey">Full-Stack Web Developer <em class="text-sm text-bronze">with</em> <span class="text-teal"> Artisan Digital Media</span> — December 2016 to August 2017</h3>
+                <ul>
+                    <li>Build custom websites using ExpressionEngine and Craft CMS</li>
+                    <li>Maintain and work with existing/legacy projects</li>
+                    <li>Build a large-scale project including complex, multi-functional features development (integrated blog for large law firm)</li>
+                </ul>
+            </article>
+            <article>
+                <h3 class="text-grey">Annotator and Geographical Researcher <em class="text-sm text-bronze">with</em> <span class="text-teal"> HERE Technologies</span> — 2012 to 2018</h3>
+                <ul>
+                    <li>Evaluate the effectiveness of HERE’s map search program by analyzing the User-entered Queries and software-provided Responses</li>
+                    <li>Research, evaluate, and communicate findings</li
+                     <li>Establish validity of software information through geographical research</li>
+                     <li>Assess performance and understanding of tagging team by supervising work and providing feedback</li>
+                     <li>Promoted to highest “tagger” supervisory position within six months and helped train several new recruits through reviewing and critiquing work</li>
+                 </ul>
+            </article>
+        </div>
     </article>
   </section>
 </template>
