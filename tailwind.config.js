af7fda7394d7b7a46eff9d9db838e2c45fff5878
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2021-10-23 13:04:46 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,19 +12,11 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		colors: {
-			'dark-purple'   : '#1B1529',
-			'medium-purple' : '#2F315A',
-			'light-purple'  : '#454EA0',
-			'dark-blue'     : '#0f084b',
-			'medium-blue'   : '#26408b',
-			'light-blue'    : '#46B4C3',
-			'cyan'          : '#19a8ae',
-			'dark-green'    : '#004643',
-			'yellow'        : '#FFDF6E',
-			'pink'          : '#EA87B6',
 			'white'         : '#FFFFFF',
-			'grey'          : '#eae8ed',
-			'black'         : '#070707',
+			'grey'          : '#3E3E3F',
+			'black'         : '#262525',
+			'teal'          : '#19A8AE',
+			'bronze'        : '#AD8D18'
 		},
 		fontFamily: {
 			'display' : ['Poiret One', 'cursive'],
