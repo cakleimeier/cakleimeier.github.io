86f8bf5681ac5291fea2940ff90a7593fc82f3ac
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2021-10-23 14:08:24 -0400

    Change tabs to spaces
    For consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-	mode: 'jit',
-	purge: {
-		content: [
-			`components/**/*.{vue,js}`,
-			`layouts/**/*.vue`,
-			`pages/**/*.vue`,
-			`plugins/**/*.{js,ts}`,
-			`nuxt.config.{js,ts}`
-		]
-	},
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		colors: {
-			'white'         : '#FFFFFF',
-			'grey'          : '#3E3E3F',
-			'black'         : '#262525',
-			'teal'          : '#19A8AE',
-			'bronze'        : '#AD8D18'
-		},
-		fontFamily: {
-			'display' : ['Poiret One', 'cursive'],
-			'sans'   : ['Montserrat', 'sans-serif'],
-			'serif'    : ['Roboto Slab', 'serif']
-		},
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+    mode: 'jit',
+    purge: {
+        content: [
+            `components/**/*.{vue,js}`,
+            `layouts/**/*.vue`,
+            `pages/**/*.vue`,
+            `plugins/**/*.{js,ts}`,
+            `nuxt.config.{js,ts}`
+        ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            'white'         : '#FFFFFF',
+            'grey'          : '#3E3E3F',
+            'black'         : '#262525',
+            'teal'          : '#19A8AE',
+            'bronze'        : '#AD8D18'
+        },
+        fontFamily: {
+            'display' : ['Poiret One', 'cursive'],
+            'sans'   : ['Montserrat', 'sans-serif'],
+            'serif'    : ['Roboto Slab', 'serif']
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
