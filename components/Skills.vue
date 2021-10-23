4f8ba6a72a33a16cafa7a0bc68e9813006c906f6
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2021-10-23 15:00:42 -0400

    Enter Skills component content

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,6 +1,69 @@
 <template>
-  <section id="skills">
-    <article>
+  <section id="skills" class="col-span-12">
+    <article class="grid grid-cols-12 gap-12">
+        <div class="col-span-12">
+            <h2>Skills</h2>
+        </div>
+
+        <div class="col-span-7">
+            <h3>Back-End</h3>
+            <p>I have experience building, maintaining, updating, and upgrading custom websites using Content Management Systems (CMS). I also have experience writing custom functionality to integrate with those CMSs. I’ve built and maintained very large websites with thousands of pages, single-page websites, and websites of every scale between.<br>To be more specific, I have experience in:</p>
+            <ul>
+                <li>Craft CMS (<em>including database setup, templating, and writing custom modules</em>)</li>
+                <li>Yii</li>
+                <li>Laravel</li>
+                <li>PHP</li>
+                <li>Relational Databases, SQL, and MySQL</li>
+                <li>Twig</li>
+            </ul>
+        </div>
+
+        <div class="col-span-5">
+            <h3>Front-End</h3>
+            <p> I have experience translating static mockups and designs into functional, fully-fledged websites. Some tools I'm proficient with include:</p>
+            <ul>
+                <li>Javascript (<em>vanilla and jquery</em>)</li>
+                <li>Vue</li>
+                <li>Various frameworks, including Foundation and Tailwind</li>
+                <li>Javascript task runners, package managers, and module bundlers (<em>including Gulp, npm, and webpack</em>)</li>
+                <li>HTML, CSS, and SCSS</li>
+            </ul>
+        </div>
+
+        <div class="col-span-7">
+            <h3>Other Skills</h3>
+
+            <ul>
+                <li><span class="font-serif tracking-wider text-xl">Communication</span><br/>
+                    <p>I excel at written communication, but am also a clear and personable speaker. I take the time to plan out my communication to be clear, concise, and effective.</p>
+                </li>
+                <li><span class="font-serif tracking-wider text-xl">Organization and Planning</span>
+                    <p> I regularly plan out projects and features, including specific details about structure and functionality and big-picture plans and processes.</p>
+                </li>
+                <li><span class="font-serif tracking-wider text-xl">Project Management</span>
+                    <p>I’ve spearheaded project management on a number of projects, from gathering requirements and clarifying expectations with clients to planning out the execution of projects with developers.</p>
+                </li>
+                <li><span class="font-serif tracking-wider text-xl">Leadership and Teamwork</span>
+                    <p>I’ve frequently guided projects and features by leading collaboration among developers and other team members, organizing and managing plans, and initiating or facilitating frequent and clear communication.</p>
+                </li>
+                <li><span class="font-serif tracking-wider text-xl">Education</span>
+                    <p>I’ve repeatedly taught end clients how to use their new CMS, from generalities about the control panel to specific features exclusive to their site.</p>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-span-5">
+            <h3>Other Technology</h3>
+            <ul>
+                <li>Git version control</li>
+                <li>Responsive websites</li>
+                <li>APIs (building and consuming)</li>
+                <li>RESTful web services</li>
+                <li>Google products (particularly Google Maps)</li>
+                <li>Mapbox</li>
+                <li>Server management with Crucial, RunCloud, DigitalOcean</li>
+            </ul>
+        </div>
     </article>
   </section>
 </template>
