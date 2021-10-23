f011606a2b68f75bcc42d64719279d6a20baa2c8
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2021-10-23 14:08:37 -0400

    Implement grid on main layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <main>
-    <About />
-    <Experience />
-    <Skills />
-    <Portfolio />
-    <Nav />
+  <main class="w-full bg-grey">
+    <div class="bg-white max-w-screen-xl mx-auto px-12 grid grid-cols-12 gap-12">
+        <About />
+        <Skills />
+        <Experience />
+        <Portfolio />
+        <Nav />
+    </div>
   </main>
-</template>
+</template>3
