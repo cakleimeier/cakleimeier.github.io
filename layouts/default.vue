58258ace0435dd333713a96f5f81ecc591e81887
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2021-10-23 18:33:37 -0400

    Edit global padding for mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,16 @@
 <template>
-  <main class="w-full bg-grey">
-    <div class="bg-white max-w-screen-xl mx-auto px-12 grid grid-cols-12 gap-12">
+  <main class="w-full">
+    <Nav />
+
+    <div class="bg-white max-w-screen-sm md:max-w-screen-md xl:max-w-screen-xl mx-auto px-2 md:px-12 grid grid-cols-12 gap-12 pt-52 md:pt-28 pb-4">
         <About />
         <Skills />
         <Experience />
         <Portfolio />
-        <Nav />
+    </div>
+
+    <div class="w-full bg-black flex align-center justify-center content-center items-center py-4">
+        <h3 class="font-display p-0">Thanks for visting!</h3>
     </div>
   </main>
 </template>
