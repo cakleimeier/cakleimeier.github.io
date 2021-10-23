b6d40fc57add3f4609baf6892933d12d0b29c608
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2021-10-23 13:05:24 -0400

    Initial About component content, layout

--- components/About.vue
+++ components/About.vue
@@ -1,6 +1,24 @@
 <template>
   <section id="about" class="">
-    <article>
+    <article class="w-full flex flex justify-around content-center">
+        <div class="w-1/4">
+
+            <figure class="image bg-grey">
+                <img class="object-contain p-4" src="~/assets/images/cakleimeier.jpg" alt="Catherine Kleimeier">
+            </figure>
+        </div>
+        <div class="w-1/2">
+            <div>
+                <h1 class="pb-0 mb-0">Catherine Kleimeier</h1>
+                <h6 class="text-bronze"><em>Rhymes with "climb higher"</em></h6>
+                <h2 class="pt-0 mt-0">Web Developer</h2>
+            </div>
+            <p>I’m a versatile, full-stack web developer living in Grand Rapids, Michigan with my wife, cat, and ever-growing collection of multi-sided dice.</p>
+
+            <p>When I’m not coding, I enjoy reading, podcasts, odd music, table-top roleplaying games, languages, and going for long walks.</p>
+
+            <p>What I love most about coding is using the magic of language to create tangible things that you can interact with and the feeling of finding a simple and elegant solution to an abstract, complex problem.</p>
+        </div>
     </article>
   </section>
 </template>
